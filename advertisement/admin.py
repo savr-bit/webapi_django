ba0da1ec1f6dc83161a263a183bb5a91f6210da1
Savr Kubanov <svkubanov@edu.hse.ru>
2024-01-26 15:00:50 +0300

    feat: add example.env + update requirements.txt

--- advertisement/admin.py
+++ advertisement/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.admin import TabularInline
+from django.urls import reverse
+from django.utils.html import format_html
 
 from advertisement.models import adv
 
@@ -8,8 +10,13 @@ from advertisement.models import adv
 #################################
 
 class ReviewInline(TabularInline): # Можно сделать, если будет необходимость
-    model = adv.User
+    model = adv.Review
     fields = ("reviews_text", "publication", "created_by", "number_of_stars", "publish_date")
+
+class AttachmentInline(TabularInline): 
+    model = adv.Attachment
+    fields = ("file", "file_type",)
+
 
 
 #################################
@@ -19,15 +26,35 @@ class ReviewInline(TabularInline): # Можно сделать, если буд�
 @admin.register(adv.Advertisement)
 class AdvertisementAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "created_by",)
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+
+    inlines = (
+        AttachmentInline,
+    )
 
 @admin.register(adv.Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "category_description",)
+    search_fields = ("category_description",)
 
 @admin.register(adv.Attachment)
 class AttachmentAdmin(admin.ModelAdmin):
-    list_display = ("id", "file_type", "file", "publication",)
+    list_display = ("id", "file_type", "file", "publication_link",)
+
+    def publication_link(self, obj):
+        link = reverse(
+            'admin:advertisement_advertisement_change', args=[obj.publication.id]
+        )
+        return format_html('<a href="{}">{}</a>', link, obj.publication)
+
 
 @admin.register(adv.Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ("id", "created_by", "number_of_stars")
+    list_display = ("id", "created_by", "number_of_stars", "review_text", "publication_link")
+
+    def publication_link(self, obj):
+        link = reverse(
+            'admin:advertisement_advertisement_change', args=[obj.publication.id]
+        )
+        return format_html('<a href="{}">{}</a>', link, obj.publication)
