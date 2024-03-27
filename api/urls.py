c9461bcb6d75727fd5933c964d8de2986b164922
Savr Kubanov <savrkubanov@MacBook-Pro-Savr.local>
2024-03-27 19:28:44 +0300

    make registation & auth

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
-from api.spectacular.urls import urlpatterns as doc_urls
 
-app_name = "api"
+from api.spectacular.urls import urlpatterns as doc_urls
+from users.urls import urlpatterns as user_urls
+
+app_name = 'api'
 
 urlpatterns = [
-    path("auth/", include("djoser.urls.jwt")),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
 urlpatterns += doc_urls
+urlpatterns += user_urls
---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from users.views import users
+
+
+
+
+urlpatterns = [
+    path('users/reg/', users.RegistrationView.as_view(), name='reg'),
+    path('users/me/', users.MeView.as_view(), name='me'),
+    path('users/change-passwd/', users.ChangePasswordView.as_view(), name='change_passwd'),
+]
+
